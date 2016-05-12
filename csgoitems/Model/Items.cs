b7fc0a655c896266d9fc7b7e36ddccdefd97f43b
Michał <dajminazwe@gmail.com>
2016-05-12 18:50:36 +0200

    Few additions - gonna change the directory soon

---
+++ csgoitems/Model/Items.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csgoitems.Model
+{
+    public class Items
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+
+        public Items(string name, string price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
