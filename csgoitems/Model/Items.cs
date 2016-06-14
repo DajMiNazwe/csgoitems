7b01f16f74c33983047d9d961132152931a8a364
Michał <dajminazwe@gmail.com>
2016-06-14 15:32:24 +0200

    App has been tested for api requests

--- csgoitems/Model/Items.cs
+++ csgoitems/Model/Items.cs
@@ -1,20 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.IO;
 
 namespace csgoitems.Model
 {
+    public class ItemsLogic
+    {
+        public async static Task<Items> GetItems(String name)
+        {
+            var http = new HttpClient();
+            var url = String.Format("http://api.csgo.steamlytics.xyz/v1/prices/{0}?key=8d800960760fe478c06f3d90e4dcee7a",name);
+            var response = await http.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+            var serializer = new DataContractJsonSerializer(typeof(Items));
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (Items)serializer.ReadObject(ms);
+
+            return data;
+
+        }
+    }
+    [DataContract]
     public class Items
     {
-        public string Name { get; set; }
-        public string Price { get; set; }
-
-        public Items(string name, string price)
-        {
-            Name = name;
-            Price = price;
-        }
+        [DataMember]
+        public bool success { get; set; }
+        [DataMember]
+        public string median_price { get; set; }
+        [DataMember]
+        public string median_net_price { get; set; }
+        [DataMember]
+        public string average_price { get; set; }
+        [DataMember]
+        public string average_net_price { get; set; }
+        [DataMember]
+        public string lowest_price { get; set; }
+        [DataMember]
+        public string message { get; set; }
+        [DataMember]
+        public string lowest_net_price { get; set; }
+        [DataMember]
+        public string highest_price { get; set; }
+        [DataMember]
+        public string highest_net_price { get; set; }
+        [DataMember]
+        public string mean_absolute_deviation { get; set; }
+        [DataMember]
+        public double deviation_percentage { get; set; }
+        [DataMember]
+        public int volume { get; set; }
+        [DataMember]
+        public int first_seen { get; set; }
     }
 }
--- csgoitems/View/SearchPage.xaml.cs
+++ csgoitems/View/SearchPage.xaml.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using csgoitems.Model;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,29 @@ namespace csgoitems.View
         public SearchPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void Observe_Click(object sender, RoutedEventArgs e)
+        {
+            String name = textBoxSearch.Text;
+            if (name != null)
+            {
+                Items myItem = await ItemsLogic.GetItems(name);
+                if(myItem.message != null)
+                {
+                    itemTextBlock.Text = ((String)myItem.message).ToString();
+                }
+                else
+                {
+                    itemTextBlock.Text = ((String)myItem.lowest_price.ToString());
+                }
+                
+            }
+        }
+
+        private void buttonSearch_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
